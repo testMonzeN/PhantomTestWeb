0fe7833ac608f3a9711a344e80b0e8661a0a4731
Karadev <elik@example.com>
2025-06-15 16:08:10 +0500

    add fix

--- Phantom/Phantom/settings.py
+++ Phantom/Phantom/settings.py
@@ -157,8 +157,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/app/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = '/app/media'
 
 AUTH_USER_MODEL = 'cabinet.User'
 
