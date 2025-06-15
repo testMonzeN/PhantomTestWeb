801b0a8f6bfc047c71c1b5e3ad102fda049ec83d
Karadev <elik@example.com>
2025-06-15 15:02:05 +0500

    :d

--- Phantom/Phantom/settings.py
+++ Phantom/Phantom/settings.py
@@ -149,8 +149,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '/app/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = '/app/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
